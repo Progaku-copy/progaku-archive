177d55f329c7ed50f6782a701d12badb040499e9
fukumura <shunsuke.fukumura@andpad.co.jp>
2024-03-16 15:52:46 +0900

    fix of rubocop

--- backend/spec/requests/tags_spec.rb
+++ backend/spec/requests/tags_spec.rb
@@ -2,19 +2,19 @@
 
 RSpec.describe 'MemosController' do
   describe 'POST /tags' do
+    subject(:endpoint) { post '/tags', params: }
+
     let(:memo) { create(:memo) }
     let(:params) { { tag: { name: 'タグ名称' }, memo_id: memo.id } }
 
-    subject { post '/tags', params: }
-
     context '正常なパラメータが送られた場合' do
       it 'tagsレコードが作成されること' do
-        expect { subject }.to change { memo.reload.tags.count }.by(1)
+        expect { endpoint }.to change { memo.reload.tags.count }.by(1)
       end
 
       it '204になること' do
         aggregate_failures do
-          subject
+          endpoint
           expect(response).to have_http_status(:no_content)
           expect(response.body).to be_empty
         end
@@ -25,12 +25,12 @@ RSpec.describe 'MemosController' do
       let(:params) { { tag: { name: '' }, memo_id: memo.id } }
 
       it 'レコードが作成されないこと' do
-        expect { subject }.to change { memo.reload.tags.count }.by(0)
+        expect { endpoint }.not_to(change { memo.reload.tags.count })
       end
 
       it 'バリデーションエラーが返ること, 422になること' do
         aggregate_failures do
-          subject
+          endpoint
           expect(response).to have_http_status(:unprocessable_entity)
           expect(response.body).to include("#{Tag.human_attribute_name(:name)}#{I18n.t('errors.messages.blank')}")
         end
@@ -41,11 +41,11 @@ RSpec.describe 'MemosController' do
       let(:params) { { tag: { name: 'タグ名称' }, memo_id: nil } }
 
       it 'レコードが作成されないこと' do
-        expect { subject }.to change { memo.reload.tags.count }.by(0)
+        expect { endpoint }.not_to(change { memo.reload.tags.count })
       end
 
       it '404が返ること' do
-        subject
+        endpoint
         expect(response).to have_http_status(:not_found)
       end
     end
