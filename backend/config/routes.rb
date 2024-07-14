e93e3090a90470bfb22da49269d4020d8b1e7fb7
shoutaro <850123.a@gmail.com>
2024-07-14 20:17:39 +0900

    memos controllerにsearchアクションを追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :tests, only: %i[index]
-  resources :memos, only: %i[index show create update destroy]
+  resources :memos, only: %i[index show create update destroy] do
+    collection do
+        get 'search'
+    end
+  end
 end
