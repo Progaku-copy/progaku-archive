12f553b71b098deba3ad8632774ec4b22ede4a2c
shoutaro <850123.a@gmail.com>
2024-08-04 21:38:22 +0900

    memos_spec.rb describe resolveﾒｿｯﾄﾞのﾃｽﾄを削除 let後の命名を変更

--- backend/spec/requests/memos_spec.rb
+++ backend/spec/requests/memos_spec.rb
@@ -156,51 +156,48 @@ RSpec.describe 'MemosController' do
   end
 
   describe 'SearchResolver' do
-    let!(:memo1) { create(:memo, title: 'テスト タイトル１', content: 'テスト コンテンツ１') }
-    let!(:memo2) { create(:memo, title: 'その他 タイトル', content: 'その他 コンテンツ') }
-    let!(:memo3) { create(:memo, title: 'テスト タイトル２', content: 'テスト コンテンツ２') }
-  
-    describe 'resolveメソッドのテスト' do
-      context 'タイトルで検索した場合' do
-        it 'タイトルフィルターが正しく機能することを確認する' do
-          filter_params = { title: 'テスト' }
-          result = Memo::SearchResolver.resolve(filter_collection: Memo.all, filter_params: filter_params)
-          expect(result).to contain_exactly(memo1, memo3)
-        end
-      end
+    let!(:test_memo_first) { create(:memo, title: 'テスト タイトル１', content: 'テスト コンテンツ１') }
+    let!(:another_title_memo) { create(:memo, title: 'その他 タイトル', content: 'その他 コンテンツ') }
+    let!(:test_memo_third) { create(:memo, title: 'テスト タイトル２', content: 'テスト コンテンツ２') }
 
-      context 'コンテンツで検索した場合' do
-        it 'コンテンツフィルターが正しく機能することを確認する' do
-          filter_params = { content: 'コンテンツ' }
-          result = Memo::SearchResolver.resolve(filter_collection: Memo.all, filter_params: filter_params)
-          expect(result).to contain_exactly(memo1, memo2, memo3)
-        end
+    context 'タイトルで検索した場合' do
+      it 'タイトルフィルターが正しく機能することを確認する' do
+        filter_params = { title: 'テスト' }
+        result = Memo::SearchResolver.resolve(filter_collection: Memo.all, filter_params: filter_params)
+        expect(result).to contain_exactly(test_memo_first, test_memo_third)
       end
+    end
 
-      context '並び替え機能のテスト' do
-        it '並び替え機能が正しく機能することを確認する' do
-          filter_params = { order: 'desc' }
-          result = Memo::SearchResolver.resolve(filter_collection: Memo.all, filter_params: filter_params)
-          expect(result).to eq([memo3, memo2, memo1])
-        end
+    context 'コンテンツで検索した場合' do
+      it 'コンテンツフィルターが正しく機能することを確認する' do
+        filter_params = { content: 'コンテンツ' }
+        result = Memo::SearchResolver.resolve(filter_collection: Memo.all, filter_params: filter_params)
+        expect(result).to contain_exactly(test_memo_first, another_title_memo, test_memo_third)
       end
+    end
 
-      context 'タイトルとコンテンツで検索した場合' do
-        it 'タイトルとコンテンツフィルターが正しく機能することを確認する' do
-          filter_params = { title: 'テスト', content: 'コンテンツ', order: 'desc' }
-          result = Memo::SearchResolver.resolve(filter_collection: Memo.all, filter_params: filter_params)
-          expect(result).to eq([memo3, memo1])
-        end
+    context '並び替え機能のテスト' do
+      it '並び替え機能が正しく機能することを確認する' do
+        filter_params = { order: 'desc' }
+        result = Memo::SearchResolver.resolve(filter_collection: Memo.all, filter_params: filter_params)
+        expect(result).to eq([test_memo_third, another_title_memo, test_memo_first])
       end
+    end
 
-      context '検索内容を入力しない場合' do
-        it '全てのメモが返されることを確認する' do
-          filter_params = {}
-          result = Memo::SearchResolver.resolve(filter_collection: Memo.all, filter_params: filter_params)
-          expect(result).to contain_exactly(memo1, memo2, memo3)
-        end
+    context 'タイトルとコンテンツで検索した場合' do
+      it 'タイトルとコンテンツフィルターが正しく機能することを確認する' do
+        filter_params = { title: 'テスト', content: 'コンテンツ', order: 'desc' }
+        result = Memo::SearchResolver.resolve(filter_collection: Memo.all, filter_params: filter_params)
+        expect(result).to eq([test_memo_third, test_memo_first])
+      end
+    end
+
+    context '検索内容を入力しない場合' do
+      it '全てのメモが返されることを確認する' do
+        filter_params = {}
+        result = Memo::SearchResolver.resolve(filter_collection: Memo.all, filter_params: filter_params)
+        expect(result).to contain_exactly(test_memo_first, another_title_memo, test_memo_third)
       end
     end
   end
 end
-
