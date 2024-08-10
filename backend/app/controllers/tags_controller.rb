ae309a189912804a2f4c2bb373a729c2bd5ab2c1
nova <173802000+nova818@users.noreply.github.com>
2024-08-10 16:50:57 +0900

    コントローラーの作成とルーティングの設定

---
+++ backend/app/controllers/tags_controller.rb
@@ -0,0 +1,49 @@
+class TafsController < ApplicationController
+  before_action :set_tag, only: [:show, :update, :destroy]
+
+  # GET /tags
+  def index
+    @tags = Tag.all
+    render json: @tags, status: :ok
+  end
+
+  # GET /tags/:id
+  def show
+    render json: @tag, status: :ok
+  end
+
+  # POST /tags
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      render json: @tag, status: :created
+    else
+      render json: { errors: @tag.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /tags/:id
+  def update
+    if @tag.update(tag_params)
+      render json: @tag
+    else
+      render json: { errors: @tag.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tags/:id
+  def destroy
+    @tag.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_tag
+    @tag = Tag.find(params[:id])
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :memos, only: %i[index show create update destroy] do
     resources :comments, only: %i[create]
   end
+
+  resources :tags, only: %i[index show create update destroy]
 end
