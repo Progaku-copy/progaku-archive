204668e115e0f773aafa7ff7656124733ea56098
nova <173802000+nova818@users.noreply.github.com>
2024-08-17 20:33:51 +0900

    フォームオブジェクトを作成

---
+++ backend/app/forms/memo_form.rb
@@ -0,0 +1,48 @@
+class MemoForm
+  include ActiveModel::Model
+
+  attr_accessor :title, :content, :tag_ids
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validate :tag_ids_must_exist
+  
+
+  def initialize(attributes = nil, memo: Memo.new)
+    @memo = memo
+    attributes = default_attributes.merge(attributes) 
+    super(attributes)
+  end
+
+  def save
+    return false if invalid?
+
+    ActiveRecord::Base.transaction do
+      memo.update!(title: title, content: content)
+      memo.memo_tags.destroy_all
+      tag_ids.each do |tag_id|
+        memo.memo_tags.create!(tag_id: tag_id)
+      end
+    end
+  end
+
+  private
+
+  attr_reader :memo
+
+  def default_attributes
+    {
+      title: memo.title,
+      content: memo.content,
+      tag_ids: memo.tags.pluck(:id)
+    }
+  end
+
+  def tag_ids_must_exist
+    return if tag_ids.blank?
+  
+    if Tag.where(id: tag_ids).count != tag_ids.size
+      errors.add(:tag_ids, "に無効なものが含まれています")
+    end
+  end
+end
