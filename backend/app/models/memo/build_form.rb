f58c5289c646da1a398eeb8451f6e0690223fe7a
nova <173802000+nova818@users.noreply.github.com>
2024-08-30 09:58:52 +0900

    フォームオブジェクトファイルの移動

---
+++ backend/app/models/memo/build_form.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+class Memo
+  class BuildForm
+    include ActiveModel::Validations
+
+    attr_reader :params
+
+    validate :memo_valid?
+
+    def initialize(params:)
+      @params = params
+    end
+
+    def memo
+      @memo ||= Memo.new(
+        title: params[:title],
+        content: params[:content]
+      )
+    end
+
+    def save
+      return false unless valid?
+
+      resolve_memo_tags
+
+      ActiveRecord::Base.transaction do
+        memo.save!
+      end
+    end
+
+    private
+
+    def memo_valid?
+      return if memo.valid? # rubocop:disable Style/ReturnNilInPredicateMethodDefinition
+
+      memo.errors.each { |error| errors.add(:base, error.full_message) }
+    end
+
+    def resolve_memo_tags
+      tags = Tag.where(id: params[:tag_ids])
+      tags.each do |tag|
+        memo.memo_tags.build(tag: tag)
+      end
+    end
+  end
+end
---
+++ backend/app/models/memo/update_form.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class Memo
+  class UpdateForm
+    include ActiveModel::Validations
+
+    attr_reader :params, :memo
+
+    validate :memo_valid?
+
+    def initialize(params:, memo:)
+      @params = params
+      @memo = memo
+    end
+
+    def update
+      return false unless valid?
+
+      resolve_memo_tags
+
+      ActiveRecord::Base.transaction do
+        memo.update!(content: params[:content])
+      end
+    end
+
+    private
+
+    def memo_valid?
+      memo.assign_attributes(content: params[:content])
+      return if memo.valid? # rubocop:disable Style/ReturnNilInPredicateMethodDefinition
+
+      memo.errors.each { |error| errors.add(:base, error.full_message) }
+    end
+
+    def resolve_memo_tags
+      memo.memo_tags.destroy_all
+      tags = Tag.where(id: params[:tag_ids])
+      tags.each do |tag|
+        memo.memo_tags.build(tag: tag)
+      end
+    end
+  end
+end
