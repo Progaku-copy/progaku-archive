10d52b6681b021d03ef5ab7b5fcb29d9f717475d
日下陸矢 <rikuya.98@gmail.com>
2024-08-31 18:03:29 +0900

    冗長なletの名前を修正

--- backend/spec/requests/comments_spec.rb
+++ backend/spec/requests/comments_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'CommentsController' do
   describe 'POST /memos/:memo_id/comments' do
     context 'コンテンツが有効な場合' do
       let(:memo) { create(:memo) }
-      let(:valid_comment_params) { { content: Faker::Lorem.paragraph(sentence_count: 3) } }
+      let(:params) { { content: Faker::Lorem.paragraph(sentence_count: 3) } }
 
       it 'コメントが追加され、204になる' do
         aggregate_failures do
           expect do
-            post "/memos/#{memo.id}/comments", params: { comment: valid_comment_params }, as: :json
+            post "/memos/#{memo.id}/comments", params: { comment: params }, as: :json
           end.to change(Comment, :count).by(1)
 
           expect(response).to have_http_status(:no_content)
@@ -20,12 +20,12 @@ RSpec.describe 'CommentsController' do
 
     context 'バリデーションエラーになる場合' do
       let(:memo) { create(:memo) }
-      let(:invalid_comment_params) { { content: '' } }
+      let(:params) { { content: '' } }
 
       it 'コメントが追加されていないこと、422になることを確認する' do
         aggregate_failures do
           expect do
-            post "/memos/#{memo.id}/comments", params: { comment: invalid_comment_params }, as: :json
+            post "/memos/#{memo.id}/comments", params: { comment: params }, as: :json
           end.not_to change(Comment, :count)
           expect(response).to have_http_status(:unprocessable_entity)
           expect(response.parsed_body['errors']).to eq(['内容を入力してください'])
@@ -38,11 +38,11 @@ RSpec.describe 'CommentsController' do
     context 'コンテンツが有効な場合' do
       let(:memo) { create(:memo) }
       let(:comment) { create(:comment, memo: memo) }
-      let(:valid_comment_params) { { content: Faker::Lorem.paragraph(sentence_count: 3) } }
+      let(:params) { { content: Faker::Lorem.paragraph(sentence_count: 3) } }
 
       it 'コメントが更新され、204になる' do
         aggregate_failures do
-          put "/memos/#{memo.id}/comments/#{comment.id}", params: { comment: valid_comment_params }, as: :json
+          put "/memos/#{memo.id}/comments/#{comment.id}", params: { comment: params }, as: :json
           expect(response).to have_http_status(:no_content)
           expect(response.body).to be_empty
         end
@@ -53,11 +53,11 @@ RSpec.describe 'CommentsController' do
   context 'バリデーションエラーになる場合' do
     let(:memo) { create(:memo) }
     let(:comment) { create(:comment, memo: memo) }
-    let(:invalid_comment_params) { { content: '' } }
+    let(:params) { { content: '' } }
 
     it 'コメントが更新されていないこと、422になることを確認する' do
       aggregate_failures do
-        put "/memos/#{memo.id}/comments/#{comment.id}", params: { comment: invalid_comment_params }, as: :json
+        put "/memos/#{memo.id}/comments/#{comment.id}", params: { comment: params }, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.parsed_body['errors']).to eq(['内容を入力してください'])
       end
@@ -67,11 +67,11 @@ RSpec.describe 'CommentsController' do
   context '存在しないコメントIDの場合' do
     let(:memo) { create(:memo) }
     let(:comment) { create(:comment, memo: memo) }
-    let(:valid_comment_params) { { content: Faker::Lorem.paragraph(sentence_count: 3) } }
+    let(:params) { { content: Faker::Lorem.paragraph(sentence_count: 3) } }
 
     it '404が返ることを確認する' do
       aggregate_failures do
-        put "/memos/#{memo.id}/comments/0", params: { comment: valid_comment_params }, as: :json
+        put "/memos/#{memo.id}/comments/0", params: { comment: params }, as: :json
         expect(response).to have_http_status(:not_found)
       end
     end
@@ -80,11 +80,11 @@ RSpec.describe 'CommentsController' do
   context '存在しないメモIDの場合' do
     let(:memo) { create(:memo) }
     let(:comment) { create(:comment, memo: memo) }
-    let(:valid_comment_params) { { content: Faker::Lorem.paragraph(sentence_count: 3) } }
+    let(:params) { { content: Faker::Lorem.paragraph(sentence_count: 3) } }
 
     it '404が返ることを確認する' do
       aggregate_failures do
-        put "/memos/0/comments/#{comment.id}", params: { comment: valid_comment_params }, as: :json
+        put "/memos/0/comments/#{comment.id}", params: { comment: params }, as: :json
         expect(response).to have_http_status(:not_found)
       end
     end
