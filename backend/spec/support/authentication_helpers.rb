003cf1ee7b71c87b0e0e2f1a505dac8ca395f671
日下陸矢 <rikuya.98@gmail.com>
2024-09-07 12:15:02 +0900

    Rspecでログイン状態にするヘルパーメソッドを定義

---
+++ backend/spec/support/authentication_helpers.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module AuthenticationHelpers
+  def sign_in(user)
+    post '/login', params: { session: { account_name: user.account_name, password: 'password' } }
+  end
+end
