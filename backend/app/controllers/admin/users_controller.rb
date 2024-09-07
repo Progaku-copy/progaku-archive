ba88ab41f65234803a81a8daffcee5b5ea5bd225
日下陸矢 <rikuya.98@gmail.com>
2024-09-07 11:47:05 +0900

    管理者のみユーザを作成できるAPIを作成

---
+++ backend/app/controllers/admin/users_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Admin
+  class UsersController < ApplicationController
+    before_action :check_admin
+    def create
+      user = User.new(user_params)
+      if user.save
+        head :no_content
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:account_name, :password, :password_confirmation)
+    end
+
+    def check_admin
+      head :forbidden unless current_user&.admin?
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users, only: %i[create]
+  end
+
   resources :memos, only: %i[index show create update destroy] do
     resources :comments, only: %i[create update destroy]
   end
