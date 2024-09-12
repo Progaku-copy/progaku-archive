c795dbd6c0d409657212449711dd449ee1699a15
日下陸矢 <rikuya.98@gmail.com>
2024-09-12 09:45:27 +0900

    authenticate_userのスキップをログイン時のみに設定

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: %i[create]
+
   def create
     user = User.find_by(account_name: session_params[:account_name])
     return render json: { errors: ['アカウント名が存在しません'] }, status: :unauthorized unless user
