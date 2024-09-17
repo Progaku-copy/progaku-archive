d27da1b57c5b94a8996595988d69c0c70275e5e1
nova <173802000+nova818@users.noreply.github.com>
2024-09-17 21:56:28 +0900

    build_form_specの修正

--- backend/spec/models/memo/build_form_spec.rb
+++ backend/spec/models/memo/build_form_spec.rb
@@ -5,73 +5,51 @@ RSpec.describe Memo::BuildForm do
   let(:tag_ids) { tags.map(&:id) }
 
   describe '#バリデーション' do
-    context 'フォームの値が有効な場合' do
-      let(:params) { { title: 'Test Memo', content: 'This is a test memo', tag_ids: tag_ids } }
-      let(:form) { described_class.new(params: params) }
+    context 'フォーム入力値が有効な場合' do
+      let(:params) { { title: 'Test Memo', content: 'This is a test memo', tag_ids: } }
+      let(:form) { described_class.new(params:) }
 
-      it '有効な状態であること' do
+      it 'フォームが有効な状態であること' do
         expect(form).to be_valid
       end
     end
 
-    context 'メモのタイトルが空文字の場合' do
-      let(:params) { { title: '', content: 'This is a test memo', tag_ids: tag_ids } }
-      let(:form) { described_class.new(params: params) }
+    context 'フォーム入力値が無効な場合' do
+      let(:params) { { title: '', content: '', tag_ids: } }
+      let(:form) { described_class.new(params:) }
 
-      it '無効な状態であること' do
-        expect(form).not_to be_valid
-      end
-
-      it '適切なエラーメッセージが追加されていること' do
-        form.valid?
-        expect(form.errors.full_messages).to eq ['タイトルを入力してください']
-      end
-    end
-
-    context 'メモの内容が空文字の場合' do
-      let(:params) { { title: 'Test Memo', content: '', tag_ids: tag_ids } }
-      let(:form) { described_class.new(params: params) }
-
-      it '無効な状態であること' do
-        expect(form).not_to be_valid
-      end
-
-      it '適切なエラーメッセージが追加されていること' do
-        form.valid?
-        expect(form.errors.full_messages).to eq ['コンテンツを入力してください']
+      it 'フォームが無効な状態であり、適切なエラーメッセージが追加されていること' do
+        aggregate_failures do
+          expect(form).not_to be_valid
+          expect(form.errors.full_messages).to eq ['メモに関連するエラーがあります']
+        end
       end
     end
   end
 
   describe '#save' do
     context 'フォームの値が有効な場合' do
-      let(:params) { { title: 'Test Memo', content: 'This is a test memo', tag_ids: tag_ids } }
-      let(:form) { described_class.new(params: params) }
+      let(:params) { { title: 'Test Memo', content: 'This is a test memo', tag_ids: } }
+      let(:form) { described_class.new(params:) }
 
-      it 'メモが新規作成されること' do
-        expect { form.save }.to change(Memo, :count).by(1)
-      end
-
-      it 'メモにタグが紐付けられること' do
-        form.save
-        expect(Memo.last.tags.pluck(:id)).to match_array(tag_ids)
-      end
-
-      it 'trueが返されること' do
-        expect(form.save).to be_truthy
+      it 'メモが新規作成され、タグが紐付けられ、trueが返されること' do
+        aggregate_failures do
+          expect { form.save }.to change { [Memo.count, MemoTag.count] }.by([1, 3])
+          expect(Memo.last.tags.pluck(:id)).to match_array(tag_ids)
+          expect(form.save).to be_truthy
+        end
       end
     end
 
     context 'フォームの値が無効な場合' do
-      let(:params) { { title: '', content: 'This is a test memo', tag_ids: tag_ids } }
-      let(:form) { described_class.new(params: params) }
+      let(:params) { { title: '', content: 'This is a test memo', tag_ids: } }
+      let(:form) { described_class.new(params:) }
 
-      it 'メモが新規作成されないこと' do
-        expect { form.save }.not_to(change(Memo, :count))
-      end
-
-      it 'falseが返されること' do
-        expect(form.save).to be_falsey
+      it 'メモとメモタグが新規作成されず、falseが返されること' do
+        aggregate_failures do
+          expect { form.save }.not_to change { [Memo.count, MemoTag.count] }
+          expect(form.save).to be_falsey
+        end
       end
     end
   end
