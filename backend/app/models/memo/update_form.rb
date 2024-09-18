31904f8caa2f838ef5260aeca13b207168bd93a1
nova <173802000+nova818@users.noreply.github.com>
2024-09-18 14:51:33 +0900

    update_formの修正

--- backend/app/models/memo/update_form.rb
+++ backend/app/models/memo/update_form.rb
@@ -5,35 +5,59 @@ class Memo
     include ActiveModel::Validations
 
     validates :memo, cascade: true
-    validates :memo_tags, cascade: true, if: -> { errors.empty? }
+    validates :memo_tags_to_add, cascade: true, if: -> { errors.empty? }
 
-    def initialize(params:, memo:)
+    def initialize(params:, id:)
       @params = params
-      @memo = memo
+      @id = id
     end
 
     def save
-      return false unless valid?
-
-      resolve_memo_tags
+      return false if invalid?
 
       ActiveRecord::Base.transaction do
-        memo.update(content: params[:content])
+        save_record!(memo)
+        memo_tags_to_remove.destroy_all
       end
 
-      true
+      errors.empty?
     end
 
     private
 
-    attr_reader :params, :memo
+    attr_reader :params, :id
 
-    def resolve_memo_tags
-      memo.memo_tags.destroy_all
-      tags = Tag.where(id: params[:tag_ids])
-      tags.each do |tag|
-        memo.memo_tags.build(tag: tag)
-      end
+    def save_record!(record)
+      return true if record.save
+
+      errors.add(:base, record.error_message)
+      raise ActiveRecord::Rollback
+    end
+
+    def memo
+      @memo ||= \
+        Memo.find(id)
+            .tap do |model|
+              model.assign_attributes(
+                title: params[:title] || model.title,
+                content: params[:content] || model.content,
+                tags: tags
+              )
+            end
+    end
+
+    def tags = Tag.where(id: params[:tag_ids])
+
+    def memo_tags_to_remove
+      current_tag_ids = memo.memo_tags.pluck(:tag_id)
+      tag_ids_to_remove = current_tag_ids - (params[:tag_ids] || [])
+      memo.memo_tags.where(tag_id: tag_ids_to_remove)
+    end
+
+    def memo_tags_to_add
+      current_tag_ids = memo.memo_tags.pluck(:tag_id)
+      tag_ids_to_add = (params[:tag_ids] || []) - current_tag_ids
+      @memo_tags_to_add = tag_ids_to_add.map { |tag_id| memo.memo_tags.build(tag_id: tag_id) }
     end
   end
 end
