f4b3cc9b94de2d353199b18f2710b90ea4b3efd3
日下陸矢 <rikuya.98@gmail.com>
2024-11-02 15:47:53 +0900

    タイムアウトはデフォルトで60秒が設定されるため条件分岐を削除

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -11,11 +11,7 @@ threads min_threads_count, max_threads_count
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
-if ENV.fetch("RAILS_ENV", "development") == "development"
-  worker_timeout 3600
-elsif ENV.fetch("RAILS_ENV", "development") == "production"
-  worker_timeout 60
-end
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
