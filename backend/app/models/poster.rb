9e1965fa8b5b23f48bdb652b1223ac481aabd3a6
日下陸矢 <rikuya.98@gmail.com>
2024-11-16 15:12:47 +0900

    Slackからuserを取得してpostersテーブルにimportするAPIを作成

--- backend/app/models/poster.rb
+++ backend/app/models/poster.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 class Poster < ApplicationRecord
-  has_many :memos
-  has_many :comments
+  has_many :memos, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :user_key, presence: true, uniqueness: true
+
+  def self.build_from_slack_posters(posters)
+    default_name = 'unknown'
+
+    posters.map do |poster|
+      next if poster['id'].blank?
+
+      new(
+        user_key: poster['id'],
+        display_name: poster.dig('profile', 'display_name').presence || default_name,
+        real_name: poster['real_name'].presence || default_name
+      )
+    end
+  end
 end
