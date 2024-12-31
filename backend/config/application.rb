dc3e6dc85f0fe33857b045dd1eb111f021471b97
日下陸矢 <rikuya.98@gmail.com>
2024-12-31 15:13:18 +0900

    設定ファイルを変更

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -30,7 +30,7 @@ module Backend
                           key: '_progaku_archive_session',
                           expire_after: 6.hours,
                           http_only: true,
-                          secure: Rails.env.production? ? true : false,
-                          same_site: :none
+                          secure: Rails.env.production?, # 本番環境でのみtrue
+                          same_site: Rails.env.production? ? :none : :lax # 本番ではnone、それ以外ではlax
   end
 end
--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store,                           key: '_progaku_archive_session',
                                        expire_after: 6.hours,
                                        http_only: true,
-                                       secure: Rails.env.production? ? true : false,
-                                       same_site: :none
+                                       secure: Rails.env.production?, # 本番環境でのみtrue
+                                       same_site: Rails.env.production? ? :none : :lax # 本番ではnone、それ以外ではlax
